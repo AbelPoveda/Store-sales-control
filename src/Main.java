a1ff6edff9ba303ca48ed59f9c76cd0ba04517e9
Thebuco1701 <Thebuco1701@users.noreply.github.com>
2020-06-09 01:48:31 +0200

    añadida estructura frame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Main {
+    private JPanel panelprincipal;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+
     public static void main(String[] args){
-        System.out.println("Inicio");
+        JFrame frame = new JFrame("Control de stock");
+        frame.setContentPane(new Main().panelprincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
